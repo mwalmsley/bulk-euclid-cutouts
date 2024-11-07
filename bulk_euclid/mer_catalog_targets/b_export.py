db9c1ba51bce9166597e0bead8cb5eacaa6d0a56
Mike Walmsley <walmsleymk1@gmail.com>
2024-11-06 21:24:28 -0500

    adjust zip

--- bulk_euclid/mer_catalog_targets/b_export.py
+++ bulk_euclid/mer_catalog_targets/b_export.py
@@ -74,11 +74,11 @@ def zip_for_download(cfg: OmegaConf):
     logging.info('Zipped catalogs')
     if cfg.jpg_outputs:
         for output_format in cfg.jpg_outputs:
-            shutil.make_archive(cfg.download_dir + f'_jpg_cutouts_{output_format}', 'zip', root_dir=cfg.jpg_dir + '/' + output_format)
+            shutil.make_archive(cfg.cutout_dir + f'_jpg_cutouts_{output_format}', 'zip', root_dir=cfg.jpg_dir + '/' + output_format)  # zips from: download/cutouts/jpg/output_format
             logging.info(f'Zipped {output_format} jpg cutouts')
         logging.info('Zipped all jpg cutouts')
     if cfg.fits_outputs:
-        shutil.make_archive(cfg.download_dir + '_fits_cutouts', 'zip', root_dir=cfg.fits_dir)
+        shutil.make_archive(cfg.cutout_dir + '_fits_cutouts', 'zip', root_dir=cfg.fits_dir)
         logging.info('Zipped fits cutouts')
 
 
