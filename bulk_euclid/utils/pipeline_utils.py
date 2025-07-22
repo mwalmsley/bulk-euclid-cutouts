8886a75ce2d67ae10339d4fd97c4e98dab784f6a
Mike Walmsley <walmsleymk1@gmail.com>
2025-07-22 17:34:22 -0400

    get/set

--- bulk_euclid/utils/pipeline_utils.py
+++ bulk_euclid/utils/pipeline_utils.py
@@ -93,8 +93,8 @@ def find_available_tiles(cfg: OmegaConf):
             mosaic.RMS = get_path_if_exists(f'{release_dir}/MER/{tile_index}/{instrument}/EUC_MER_RMS-{band}_{tile_index}-*.fits')
         
             # for now, only use if all required data products exist
-            if all([mosaic.__dict__[key] is not None for key in [cfg.data_products]]):  # e.g. BGSUB, BGMOD, RMS
-                tile.__dict__[band] = mosaic
+            if all([getattr(mosaic, key, False) for key in [cfg.data_products]]):  # e.g. BGSUB, BGMOD, RMS. String is Truthy.
+                setattr(tile, band, mosaic)
             else:
                 logging.warning(f'Skipping mosaic as not all data products exist, for tile {tile_index}, instrument {instrument}, band {band}: {mosaic}')
         tiles.append(tile)
